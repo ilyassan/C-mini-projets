72a1feaeefc2ec663d8c335937b33e92a8f08672
ilyassanida <anidailyass@gmail.com>
2024-09-04 10:05:47 +0100

    Le 4 challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int len;
+    printf("Entrer le nombre des elements: ");
+    scanf("%d", &len);
+
+    int t[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("\nEntrer le %d numero: ", i + 1);
+        scanf("%d", &t[i]);
+    }
+    
+    int max = -__INT16_MAX__;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (t[i] > max)
+        {
+            max = t[i];
+        }
+    }
+
+    printf("\nLa maximum valeur est: %d\n", max);
+}
