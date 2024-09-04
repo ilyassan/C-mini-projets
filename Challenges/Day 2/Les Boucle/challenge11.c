04d0d5738f21c32df53faadad7928014c895dd67
ilyassanida <anidailyass@gmail.com>
2024-09-04 17:05:12 +0100

    Le 11 challenge (Les boucles).

---
+++ Challenges/Day 2/Les Boucle/challenge11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int t[] = {2, 5, 4, 6, 0};
+    int len = sizeof(t) / sizeof(t[0]);
+
+    float somme = 0;
+    int zeros = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (t[i] != 0)
+        {
+            somme += t[i];
+            continue;
+        }
+        zeros++;
+    }
+
+    float moyenne = somme / (len - zeros);
+
+    printf("Le moyenne est: %.2f", moyenne);
+    
+}
