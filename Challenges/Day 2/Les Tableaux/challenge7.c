24aea233ab8924104ffb19ee6e680b48bc3b1109
ilyassanida <anidailyass@gmail.com>
2024-09-04 11:14:13 +0100

    Le 7 challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    int len;
+    printf("Entrer le nombre des elements: ");
+    scanf("%d", &len);
+
+    int t[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("\nEntrer le %d numero: ", i + 1);
+        scanf("%d", &t[i]);
+    }
+
+    // Bubble sort
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - 1; j++)
+        {
+            if (t[j] > t[j + 1])
+            {
+                int temp = t[j];
+                t[j] = t[j + 1];
+                t[j + 1] = temp;
+            }
+            
+        }
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i + 1 == len)
+        {
+            printf("%d", t[i]);
+            break;
+        }
+        printf("%d => ", t[i]);
+    }
+}
