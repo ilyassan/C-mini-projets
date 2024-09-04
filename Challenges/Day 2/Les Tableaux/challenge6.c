8823c339b3b38fa1ddcbd771750f0068b451c708
ilyassanida <anidailyass@gmail.com>
2024-09-04 10:29:23 +0100

    Le 6 challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+    int len;
+    printf("Entrer le nombre des elements: ");
+    scanf("%d", &len);
+
+    int t[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("\nEntrer le %d numero: ", i + 1);
+        scanf("%d", &t[i]);
+    }
+    
+    int facteur;
+    printf("\nViellez entrer le facteur: ");
+    scanf("%d", &facteur);
+
+    for (int i = 0; i < len; i++)
+    {
+        t[i] = facteur * t[i];
+    }
+
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i + 1 == len)
+        {
+            printf("%d", t[i]);
+            break;
+        }
+        printf("%d => ", t[i]);
+    }
+}
