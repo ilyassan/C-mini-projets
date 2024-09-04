07a5d2ba5ed7383588b92fad57d8d14d52fcda09
ilyassanida <anidailyass@gmail.com>
2024-09-04 11:19:05 +0100

    Le 8 challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void printTableux(int t[], int len);
+
+int main(){
+
+    int t[] = { 2, 4, 6, 1};
+    int len = sizeof(t) / sizeof(t[0]);
+
+    int copyT[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        copyT[i] = t[i];
+    }
+
+    printTableux(t, len);
+    printf("\n");
+    printTableux(copyT, len);
+}
+
+
+void printTableux(int t[], int len){
+    for (int i = 0; i < len; i++)
+    {
+        if (i + 1 == len)
+        {
+            printf("%d", t[i]);
+            break;
+        }
+        printf("%d => ", t[i]);
+    }
+}
