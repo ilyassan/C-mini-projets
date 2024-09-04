0863820859b0960c467061d06cfaeedc6d2cb41e
ilyassanida <anidailyass@gmail.com>
2024-09-04 11:46:18 +0100

    Le 13 challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int len;
+    printf("Entrer le nombre des elements: ");
+    scanf("%d", &len);
+
+    int t[len];
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("\nEntrer le %d numero: ", i + 1);
+        scanf("%d", &t[i]);
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        if (t[i] % 2 == 1)
+        {
+            printf("=> %d ", t[i]);
+        }
+    }
+}
