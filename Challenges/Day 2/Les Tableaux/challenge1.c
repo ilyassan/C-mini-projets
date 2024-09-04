5ddfc4cfc0d2c33233deee4ea9c2fc5ff447cee8
ilyassanida <anidailyass@gmail.com>
2024-09-04 09:47:13 +0100

    Le premier challenge.

---
+++ Challenges/Day 2/Les Tableaux/challenge1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+    int t[] = {1, 2, 3, 4, 6};
+    int len = sizeof(t) / sizeof(t[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (i + 1 == len)
+        {
+            printf("%d", t[i]);
+            break;
+        }
+        printf("%d => ", t[i]);
+    }
+}
