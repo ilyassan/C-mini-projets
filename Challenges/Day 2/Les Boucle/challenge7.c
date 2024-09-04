93b6f01a31c64436642f6f6d6ef9e5874ea01a42
ilyassanida <anidailyass@gmail.com>
2024-09-04 14:12:45 +0100

    Le 7 challenge (Les boucles).

---
+++ Challenges/Day 2/Les Boucle/challenge7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("La somme fibanoci de 1 - n (Entrer n): ");
+    scanf("%d", &n);
+
+    int somme = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        somme += i;
+    }
+
+
+    printf("La somme de fibanoci est: %d", somme);
+}
