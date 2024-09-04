195e2f192fba5f76bce1ba749a73dfca561204ef
ilyassanida <anidailyass@gmail.com>
2024-09-04 09:51:51 +0100

    Le deuxième challenge ( Tableux).

---
+++ Challenges/Day 2/Les Tableaux/challenge2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(){
+    int t[5];
+    int len = sizeof(t) / sizeof(t[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("\nEntrer le %d numero: ", i + 1);
+        scanf("%d", &t[i]);
+    }
+    
+    for (int i = 0; i < len; i++)
+    {
+        if (i + 1 == len)
+        {
+            printf("%d", t[i]);
+            break;
+        }
+        printf("%d => ", t[i]);
+    }
+}
