bb7a72d9adefffd9a267564aa740895d3dc5e90b
ilyassanida <anidailyass@gmail.com>
2024-09-04 12:47:39 +0100

    Le 4 challenge (Les boucles).

---
+++ Challenges/Day 2/Les Boucle/challenge4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("Entrer un entier pour reverser: ");
+    scanf("%d", &n);
+
+    int rev = 0;
+    while (n)
+    {
+        rev = (rev * 10) + (n % 10);
+        n /= 10;
+    }
+
+    printf("La resultat est: %d", rev);
+}
