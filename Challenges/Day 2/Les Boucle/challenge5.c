c9510b928d1bfee6385d43af83a042360eab561e
ilyassanida <anidailyass@gmail.com>
2024-09-04 12:53:08 +0100

    Le 5 challenge (Les boucles).

---
+++ Challenges/Day 2/Les Boucle/challenge5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("La somme des nombres entre 1 - n, entrer n: ");
+    scanf("%d", &n);
+
+    int somme = 0;
+
+    // Method 1:
+    // somme = n * (n + 1) / 2;
+    // printf("La somme est: %d", somme);
+
+    // Method 2 (Avec les boucles) :
+    for (int i = 1; i <= n; i++)
+    {
+        somme += i;
+    }
+    
+    printf("La somme est: %d", somme);
+}
