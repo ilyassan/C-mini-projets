e0676110f726a7ee25c279afcbc6cb7a6160b15f
ilyassanida <anidailyass@gmail.com>
2024-09-06 19:23:02 +0100

    Le 5 challenge (Algo).

--- Challenges/DAY 4/Algorihtms/challenge4.c
+++ Challenges/DAY 4/Algorihtms/challenge4.c
@@ -60,36 +60,3 @@ void printTableux(int t[], int len){
         printf("=> %d ", t[i]);
     }
 }
-
-
-
-
-// void triRapide(int t[], int debut, int fin){
-//     if (debut >= fin) return;
-    
-//     int pivot = obtenuPivot(t, debut, fin);
-//     triRapide(t, debut, pivot - 1);
-//     triRapide(t, pivot + 1, fin);
-// }
-
-// int obtenuPivot(int t[], int debut, int fin){
-//     int pivot = t[fin];
-
-//     int i = debut - 1;
-//     for (int j = debut; j < fin - 1; j++)
-//     {
-//         if (t[j] < t[pivot])
-//         {
-//             i++;
-//             int temp = t[i];
-//             t[i] = t[j];
-//             t[j] = temp;
-//         }   
-//     }
-//     i++;
-//     int temp = t[i];
-//     t[i] = t[fin];
-//     t[fin] = temp;
-    
-//     return i;
-// }
---
+++ Challenges/DAY 4/Algorihtms/challenge5.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <time.h>
+
+
+void printTableux(int t[], int len);
+void triFusion(int t[], int len);
+void fusion(int tableuxGauche[], int tableuxDroit[], int t[], int len);
+
+int main(){
+
+    int t[] = {3, 5, 3, 1, 8, 5};
+    int len = sizeof(t) / sizeof(t[0]);
+
+    puts("Avant");
+    printTableux(t, len);
+    
+    // Tri Rapide
+    triFusion(t, len);
+
+    puts("\nApres");
+    printTableux(t, len);
+
+    return 0;
+}
+
+void triFusion(int t[], int len){
+    
+    if (len <= 1) return; // cas de base
+    
+    int middle = len / 2;
+
+    int tableuxGauche[middle];
+    int tableuxDroit[len - middle];
+
+    for (int i = 0; i < middle; i++)
+    {
+        tableuxGauche[i] = t[i];
+    }
+    for (int i = 0; i < len - middle; i++)
+    {
+        tableuxDroit[i] = t[i + middle];
+    }
+    
+    triFusion(tableuxGauche, middle);
+    triFusion(tableuxDroit, len - middle);
+    fusion(tableuxGauche, tableuxDroit, t, len);
+}
+
+void fusion(int tableuxGauche[], int tableuxDroit[], int t[], int len){
+    int lenGauche = len / 2;
+    int lenDroit = len - lenGauche;
+
+    int i = 0, g = 0, d = 0; // les indices
+
+    while (g < lenGauche && d < lenDroit)
+    {
+        if (tableuxGauche[g] < tableuxDroit[d])
+        {
+            t[i] = tableuxGauche[g];
+            i++;
+            g++;
+        }
+        else{
+            t[i] = tableuxDroit[d];
+            i++;
+            d++;
+        }
+    }
+
+    while (g < lenGauche)
+    {
+        t[i] = tableuxGauche[g];
+        i++;
+        g++;
+    }
+    while (d < lenDroit)
+    {
+        t[i] = tableuxDroit[d];
+        i++;
+        d++;
+    }
+    
+}
+
+
+
+void printTableux(int t[], int len){
+    for (int i = 0; i < len; i++)
+    {
+        printf("=> %d ", t[i]);
+    }
+}
